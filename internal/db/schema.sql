5bc6ea644d5e83cddb66470d158bd1cc9d74dee6
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2023-03-04 19:01:05 +0500

    fix: add default values to database

--- internal/db/schema.sql
+++ internal/db/schema.sql
@@ -6,17 +6,17 @@ CREATE TABLE transactions (
 	request_service varchar(255),
 	request_phone varchar(255),
 	request_amount varchar(255),
-	status varchar(255),
-	error_code int,
-	error_msg varchar(255),
-	result_status varchar(255),
-	result_ref_num bigint,
-	result_service varchar(255),
-	result_destination varchar(255),
-	result_amount bigint,
-	result_state varchar(255),
+	status varchar(255) DEFAULT '',
+	error_code int DEFAULT 0,
+	error_msg varchar(255) DEFAULT '',
+	result_status varchar(255) DEFAULT '',
+	result_ref_num bigint DEFAULT 0,
+	result_service varchar(255) DEFAULT '',
+	result_destination varchar(255) DEFAULT '',
+	result_amount bigint DEFAULT 0,
+	result_state varchar(255) DEFAULT '',
 	is_checked boolean NOT NULL DEFAULT false,
-	client varchar(255)
+	client varchar(255) NOT NULL
 );
 
 CREATE TABLE users (
